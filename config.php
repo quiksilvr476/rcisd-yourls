c6ae7436b7d0eda505f3f150835235ed1cca5339
Ryan Knauth <quiksilvr476@gmail.com>
2017-11-08 04:23:21 -0600

    Update config.php

--- config.php
+++ config.php
@@ -5,16 +5,16 @@
 // GENERAL
 
 // Site URL
-define('siteURL', 'http://localhost:8888/');
+define('siteURL', 'http://rcurl.org');
 
 // Page title for your site
-define('title', 'Sleeky theme for YOURLS'); 
+define('title', 'RCISD URL Shortener'); 
 
 // The short title of your site, used in the footer and in some sub pages
-define('shortTitle', 'Sleeky');
+define('shortTitle', 'RCISD Short URL');
 
 // A description of your site, shown on the homepage.
-define('description', 'A quick description on why your site is so fantastic, what it does and why people should definetly start using it. Oh, and how it’s free.'); 
+define('description', 'Provides a custom URL shortening service for RCISD staff '); 
 
 // The favicon for your site
 define('favicon', '/assets/img/favicon.ico');
@@ -29,9 +29,9 @@ define('logo', '/assets/img/logo-black.png');
 // The array follows a title link structure:
 // "TITLE" => "LINK",
 $footerLinks = [
-    "About" => "http://google.com",
-    "Contact" => "http://apple.com",
-    "Legal" => "http://example.com",
+    "District Website" => "https://www.rcisd.org/",
+    "Documentation" => "https://rcisd.freshservice.com/support/solutions",
+    "Support" => "https://rcisd.freshservice.com/support/tickets/new",
 ];
 
 ?>
